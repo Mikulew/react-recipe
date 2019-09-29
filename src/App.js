0acf0cc873c386fed7c6c2b9e9094d2e182ada64
Mikulew <mikulew@gmail.com>
2019-09-29 14:29:50 +0200

    Add APP_KEY from food2fork, fetching and displaying the recipes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,38 @@ import './App.css';
 
 import Form from './components/Form';
 
+const API_KEY = 'e81856a666ddf1bac99b061dd53082c5';
+
 class App extends Component {
-  getRecipe = e => {
+  state = {
+    recipes: [],
+  };
+
+  getRecipe = async e => {
     e.preventDefault();
     const recipeName = e.target.elements.recipeName.value;
-    console.log('TCL: App -> recipeName', recipeName);
+    const data = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}`);
+    const results = await data.json();
+    this.setState({ recipes: results.recipes });
   };
 
   render() {
+    const { recipes } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">React Recipe</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        {recipes.map(recipe => {
+          return (
+            <div key={recipe.recipe_id}>
+              <img src={recipe.image_url} alt={recipe.title} />
+              <p>{recipe.title}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
